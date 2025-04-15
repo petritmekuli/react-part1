b90308d13bb0b38123262593b117deb657ab8055
petritmekuli <petritmekuli2013@gmail.com>
2025-04-16 00:05:26 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import "./App.css";
+
+import ProductForm from "./components/ProductForm";
+
+function App() {
+  return (
+    <>
+      <ProductForm />
+    </>
+  );
+}
+
+export default App;
